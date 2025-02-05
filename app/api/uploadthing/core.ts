a43eb04d983497bf0130d8c2964e2a115221dd45
sarimAbdelbari <sarimabdelbari@gmail.com>
2025-02-05 16:45:00 +0100

    i integreated a files storage system using uploadthing platform

---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,34 @@
+import { auth } from "@/lib/auth";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+
+const f = createUploadthing();
+
+
+
+export const ourFileRouter = {
+
+    fileUploader: f({
+    image: {   
+      maxFileSize: "4MB",
+      maxFileCount: 1,
+    },
+  }).middleware(async () => {
+
+      const session = await auth();
+
+      if (!session?.user) throw new UploadThingError("Unauthorized");
+
+      // Whatever is returned here is accessible in onUploadComplete as `metadata`
+      return { userId: session?.user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+
+      console.log("Upload complete for userId:", metadata.userId);
+      console.log("file url", file.url);
+
+      return { uploadedBy: metadata.userId };
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
--- components/customUi/uploadFile.tsx
+++ components/customUi/uploadFile.tsx
@@ -4,23 +4,47 @@ import { Button } from '@/components/ui/button'
 import { Dialog, DialogContent, DialogTrigger, DialogTitle } from '@/components/ui/dialog'
 import Dropzone from 'react-dropzone'
 import { Cloud, File } from 'lucide-react'
+import { UploadButton } from '@/components/utils/uploadthing'
+import { successToast , errorToast} from '@/components/utils/toastNotification'
+import { Progress } from '@/components/ui/progress'
 
 const UploadDropZone = () => {
+ 
+    const [isUploading , setIsUploading] =useState(null);
+
     return (
         <Dropzone multiple={false} onDrop={(acceptedFiles) => console.log(acceptedFiles)}>
          {({getRootProps, getInputProps , acceptedFiles}) => (
              <div {...getRootProps()} className='border h-64 m-4 border-dashed border-gray-300 rounded-lg p-4 bg-muted flex flex-col items-center justify-center gap-4'>
                 <div className='flex items-center justify-center  w-full'>
-                    <label htmlFor='dropzone-file' className='flex flex-col items-center justify-center w-full h-full cursor-move rounded-lg'>
+                    <label htmlFor='dropzone-file' className='flex flex-col items-center justify-center w-full h-full cursor-pointer rounded-lg'>
                         <div className='flex flex-col items-center justify-center '>
                           <Cloud/>
+                          <UploadButton 
+                          endpoint="fileUploader"
+                          onUploadProgress={(progress) => {
+                            setIsUploading(progress)
+                            console.log(`Upload Progress: ${progress}%`)
+                          }}
+                          onClientUploadComplete={(res) => {
+                            // // Do something with the response
+                            console.log("Files: ", res);
+                            successToast("Upload Completed");
+                          }}
+                          onUploadError={(error: Error) => {
+                            // // Do something with the error.
+                            errorToast(`Error! ${error.message}`)
+                          }} 
+                          />
                           <p className='my-2 text-sm text-zinc-700'>
                             <span className='font-semibold text-foreground'>
                             Drag or Click To Upload The File
                             </span>{' '}
                            </p>
-                            <p className='text-xs text-muted-foreground'>Only PDF files are allowed Size (5 Mb)</p>
+                            <p className='text-xs text-muted-foreground'>Only PDF files are allowed Size (4 Mb)</p>
+                            <Progress value={isUploading}/>
                         </div>
+                        
                     </label>
                 </div>
 
@@ -32,7 +56,7 @@ const UploadDropZone = () => {
                         <div className=' px-3 py-2 h-full truncate border  text-sm font-semibold '>{acceptedFiles[0].name}</div>
                     </div>
                 ) : null}
-             </div>
+             </div> 
          )}
         </Dropzone>
     )
